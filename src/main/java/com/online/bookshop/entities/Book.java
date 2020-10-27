7bb8520d3c84e41a47c9085aa3d9b2d28ae8eeeb
shermi.maleesha <shermi.maleesha@inovaitsys.com>
2020-10-28 00:58:31 +0530

    entities

---
+++ src/main/java/com/online/bookshop/entities/Book.java
@@ -0,0 +1,8 @@
+package com.online.bookshop.entities;
+
+public class Book {
+	private Long id;
+	private String name;
+	private Long categoryId;
+
+}
---
+++ src/main/java/com/online/bookshop/entities/BookCategory.java
@@ -0,0 +1,8 @@
+package com.online.bookshop.entities;
+
+public class BookCategory {
+	private Long id;
+	private String categoryName;
+	private String categoryCode;
+
+}
